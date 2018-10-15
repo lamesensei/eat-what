43e1e1208fb7c9d4e81a040f1705da9330b1e1d9
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-15 15:21:13 +0800

    Update db.js for heroku

--- db.js
+++ db.js
@@ -1,13 +1,29 @@
 const pg = require('pg');
+const url = require('url');
 const user = require('./models/user');
 const food = require('./models/food');
 
-const configs = {
-  user: 'lamesensei',
-  host: '127.0.0.1',
-  database: 'eatwhatdb',
-  port: 5432,
-};
+let configs;
+if (process.env.DATABASE_URL) {
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true,
+  };
+} else {
+  configs = {
+    user: 'lamesensei',
+    host: '127.0.0.1',
+    database: 'eatwhatdb',
+    port: 5432,
+  };
+}
 
 const pool = new pg.Pool(configs);
 
